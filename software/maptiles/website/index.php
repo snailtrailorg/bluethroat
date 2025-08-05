ad04e8e0dda10ce8914f51d437c2181b3e84bf7f
michael <michael@snailtrail.org>
2025-08-05 12:57:23 +0800

    htaccess

--- software/maptiles/website/index.php
+++ software/maptiles/website/index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ . '/utils/phpseclib/vendor/autoload.php';
+    require_once __DIR__ . '/modules/phpseclib/vendor/autoload.php';
     require_once __DIR__ . '/database.php';
 
     use phpseclib3\Crypt\PublicKeyLoader;
